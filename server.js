6543c0f73439a16f496297f2703f5bd22fb7b253
Ghcat0528 <fcatalanog7@gmail.com>
2025-03-04 22:29:25 -0700

    maybe

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: '*',  
+  origin: 'https://beautiful-conkies-4d2965.netlify.app',  
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
